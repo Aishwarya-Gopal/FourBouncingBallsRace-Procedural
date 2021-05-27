86b835b29f15eb79c987005ef4fdcd473a1edf6e
aishwarya-g-thoughtworks <aishwarya.g@thoughtworks.com>
2021-05-28 00:39:57 +0530

    Refactoring the draw method

--- src/main/java/FourBallsRace.java
+++ src/main/java/FourBallsRace.java
@@ -4,7 +4,7 @@ public class FourBallsRace extends PApplet {
     private final int SKETCH_BACKGROUND_COLOR = 0;
     private final int SKETCH_WIDTH = 640;
     private final int SKETCH_HEIGHT = 480;
-    private final int BALL_RADIUS = 25;
+    private final int BALL_DIAMETER = 25;
     private int ballOneXCoor, ballOneYCoor;
 
     public static void main(String[] args) {
@@ -26,17 +26,9 @@ public class FourBallsRace extends PApplet {
 
     @Override
     public void draw() {
-        setColorAndMove(235, 52, 52, ballOneXCoor, ballOneYCoor);
-    }
-
-    public void setColorAndMove(int R, int G, int B, int x_coor, int y_coor) {
-        fill(R, G, B);
-        ellipse(x_coor, y_coor, BALL_RADIUS, BALL_RADIUS);
-        moveTheBall();
-    }
-
-    private void moveTheBall() {
-        ballOneXCoor += 1;
+        fill(235, 52, 52);
+        ellipse(ballOneXCoor, ballOneYCoor, BALL_DIAMETER,BALL_DIAMETER);
+        ballOneXCoor++;
     }
 
     private void setBlackBackground() {
