f914e103c108234f851f57fc0bc208942a621e2c
aishwarya-g-thoughtworks <aishwarya.g@thoughtworks.com>
2021-05-28 00:31:32 +0530

    Refactored to create method for set color and move

--- src/main/java/FourBallsRace.java
+++ src/main/java/FourBallsRace.java
@@ -4,7 +4,8 @@ public class FourBallsRace extends PApplet {
     private final int SKETCH_BACKGROUND_COLOR = 0;
     private final int SKETCH_WIDTH = 640;
     private final int SKETCH_HEIGHT = 480;
-    private int ballOneXCoor;
+    private final int BALL_RADIUS = 25;
+    private int ballOneXCoor, ballOneYCoor;
 
     public static void main(String[] args) {
         PApplet.main("FourBallsRace", args);
@@ -19,12 +20,18 @@ public class FourBallsRace extends PApplet {
     @Override
     public void setup() {
         setBlackBackground();
+        ballOneXCoor = 0;
+        ballOneYCoor = SKETCH_HEIGHT / 5;
     }
 
     @Override
     public void draw() {
-        fill(235, 52, 52);
-        ellipse(ballOneXCoor, SKETCH_HEIGHT / 5, 25, 25);
+        setColorAndMove(235, 52, 52, ballOneXCoor, ballOneYCoor);
+    }
+
+    public void setColorAndMove(int R, int G, int B, int x_coor, int y_coor) {
+        fill(R, G, B);
+        ellipse(x_coor, y_coor, BALL_RADIUS, BALL_RADIUS);
         moveTheBall();
     }
 
