9f9a97edf24f4d25cbc30b08bbb0ebec334e674e
aishwarya-g-thoughtworks <aishwarya.g@thoughtworks.com>
2021-05-28 00:44:30 +0530

    Got the second ball rolling

--- src/main/java/FourBallsRace.java
+++ src/main/java/FourBallsRace.java
@@ -1,11 +1,11 @@
 import processing.core.PApplet;
 
 public class FourBallsRace extends PApplet {
-    private final int SKETCH_BACKGROUND_COLOR = 0;
-    private final int SKETCH_WIDTH = 640;
     private final int SKETCH_HEIGHT = 480;
     private final int BALL_DIAMETER = 25;
     private int ballOneXCoor, ballOneYCoor;
+    private int ballTwoXCoor;
+    private int ballTwoYCoor;
 
     public static void main(String[] args) {
         PApplet.main("FourBallsRace", args);
@@ -14,6 +14,7 @@ public class FourBallsRace extends PApplet {
     @Override
     public void settings() {
         super.settings();
+        int SKETCH_WIDTH = 640;
         size(SKETCH_WIDTH, SKETCH_HEIGHT);
     }
 
@@ -22,16 +23,23 @@ public class FourBallsRace extends PApplet {
         setBlackBackground();
         ballOneXCoor = 0;
         ballOneYCoor = SKETCH_HEIGHT / 5;
+        ballTwoXCoor = 0;
+        ballTwoYCoor = (SKETCH_HEIGHT * 2) / 5;
     }
 
     @Override
     public void draw() {
         fill(235, 52, 52);
-        ellipse(ballOneXCoor, ballOneYCoor, BALL_DIAMETER,BALL_DIAMETER);
+        ellipse(ballOneXCoor, ballOneYCoor, BALL_DIAMETER, BALL_DIAMETER);
         ballOneXCoor++;
+
+        fill(79, 255, 56);
+        ellipse(ballTwoXCoor, ballTwoYCoor, BALL_DIAMETER, BALL_DIAMETER);
+        ballTwoXCoor+=2;
     }
 
     private void setBlackBackground() {
+        int SKETCH_BACKGROUND_COLOR = 0;
         background(SKETCH_BACKGROUND_COLOR);
     }
 }
